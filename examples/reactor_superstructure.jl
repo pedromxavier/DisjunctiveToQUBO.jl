3984278e49dd57684641bc95223247f135a25bdc
pulsipher <joshuapulsipher13@gmail.com>
2023-11-18 17:40:45 -0500

    simplify disjunction

--- examples/reactor_superstructure.jl
+++ examples/reactor_superstructure.jl
@@ -11,15 +11,15 @@ d = 3 # not specified in the problem, I think it is 3
 model = GDPModel(BARON.Optimizer) # TODO replace with ToQUBO
 
 # Add the variables
-@variable(model, x[1:2] >= 0)
+@variable(model, 0 <= x[1:2] <= 5)
 @variable(model, y[1:2], Logical)
 
 # Add the objective
 @objective(model, Min, cx' * x.^2 + cy' * binary_variable.(y))
 
 # Create the disjunction constraint
-@constraint(model, [i = 1:2], x[i] <= 5, Disjunct(y[i]))
-@constraint(model, [(i, j) = [(1, 2), (2, 1)]], x[i] <= 0, Disjunct(y[j]))
+@constraint(model, x[2] <= 0, Disjunct(y[1]))
+@constraint(model, x[1] <= 0, Disjunct(y[2]))
 disjunction(model, y, exactly1 = true) # explcitly show the keyword to add an exactly 1 constraint 
 
 # Add the global constraint
